8af258a34daf51b5c102e96de11c49407a49fb4f
Tobias Grott <tobiasgrott@gmail.com>
2020-03-26 14:37:13 +0100

    created state for jule screenshot

--- lib/main.dart
+++ lib/main.dart
@@ -61,51 +61,43 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-// This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
+
+    final List<String> players = <String>[
+      "1.1 Mert, Kenderici",
+      "1.2 Jung, Felix",
+      "1.3 Schuler, Lukas",
+      "1.4 Hofmann, Jonah",
+      "2.1 Schlagmüller, Noah",
+      "2.2 Heßenauer, Daniel",
+      "2.3 Öttinger, Luis",
+      "2.4 Burkhardt, Daniel",
+      "2.5 Kaiser, Lennox",
+      "2.6 Separautzki, Maximilian",
+      "2.7 Anhalt, Leon",
+      "2.8 Angerbauer, Lasse",
+      "2.9 Schuler, Tim",
+      "2.10 Wasser, Julian",
+      "2.11 Kochsmeier, Enes"
+    ];
+    return Scaffold(
+      appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
+        title: Text(widget.title),
       ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
+      body: ListView.builder(
+              padding: const EdgeInsets.all(8),
+              itemCount: players.length,
+              itemBuilder: (BuildContext context, int index) {
+              return Container(
+                height: 50,
+                child: Container(child: Text(players[index]))
+              );
+      }),
+      floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: new Icon(Icons.add),
+        child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
